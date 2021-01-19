6a434f65e60f52c6a1bdadfb63bd64cdfd7c041c
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-19 15:01:23 +0200

    Exercises

--- 02_stacks_and_queues_ex/src/Exercise_01.java
+++ 02_stacks_and_queues_ex/src/Exercise_01.java
@@ -1,9 +1,14 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise_01 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		String line = scanner.nextLine();
+		ArrayDeque<String> numbers = new ArrayDeque<>();
+		Arrays.stream(scanner.nextLine().split("\\s+")).forEach(e -> numbers.push(e));
+
+		numbers.forEach(num -> System.out.print(numbers.pop()+" "));
 	}
 }
