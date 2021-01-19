57d2a2c7c67097751512123aa10e6d704c4985ca
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-19 14:20:07 +0200

    Exercises

--- 02_stacks_and_queues_ex/src/Exercise_03.java
+++ 02_stacks_and_queues_ex/src/Exercise_03.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 
 public class Exercise_03 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		String line = scanner.nextLine();
 	}
 }
--- 02_stacks_and_queues_ex/src/Exercise_10.java
+++ 02_stacks_and_queues_ex/src/Exercise_10.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 
 public class Exercise_10 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		String line = scanner.nextLine();
 	}
 }
