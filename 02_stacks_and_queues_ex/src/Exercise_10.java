93e446e8928e87b2f256243a541617eecc37d18e
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-19 17:30:57 +0200

    Exercises

--- 02_stacks_and_queues_ex/src/Exercise_10.java
+++ 02_stacks_and_queues_ex/src/Exercise_10.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise_10 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		int numberOfPlants = Integer.parseInt(input.nextLine());
+		String[] plantsArr = input.nextLine().split(" ");
+		input.close();
 
-		String line = scanner.nextLine();
+		ArrayList<Integer> plants = new ArrayList<>();
+		for (int i = 0; i < numberOfPlants; i++) {
+			plants.add(Integer.parseInt(plantsArr[i]));
+		}
+		boolean plantDied = true;
+		int day = 0;
+
+		ArrayList<Integer> deadPlants = new ArrayList<>();
+
+		while (plantDied) {
+			deadPlants = new ArrayList<>();
+			day++;
+			plantDied = false;
+			for (int i = 1; i < plants.size(); i++) {
+				if (plants.get(i - 1) < plants.get(i)) {
+					plantDied = true;
+					deadPlants.add(i);
+				}
+			}
+			for (int i = 0; i < deadPlants.size(); i++) {
+				plants.set(deadPlants.get(i), Integer.MAX_VALUE);
+			}
+			plants.removeAll(Arrays.asList(Integer.MAX_VALUE));
+
+		}
+		System.out.println(--day);
 	}
 }
