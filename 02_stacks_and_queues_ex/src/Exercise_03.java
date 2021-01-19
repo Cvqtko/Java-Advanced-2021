aab84b951bb6857e6048c988c7b5b3373df7e633
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-19 15:36:01 +0200

    Exercises

--- 02_stacks_and_queues_ex/src/Exercise_03.java
+++ 02_stacks_and_queues_ex/src/Exercise_03.java
@@ -1,9 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Exercise_03 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		String line = scanner.nextLine();
+		int countCommands = Integer.parseInt(scanner.nextLine());
+		ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+		for (int i = 0; i < countCommands; i++) {
+			String[] commands = scanner.nextLine().split("\\s+");
+
+			String command = commands[0];
+
+			switch (command) {
+			case "1":
+				int number = Integer.parseInt(commands[1]);
+				stack.push(number);
+				break;
+			case "2":
+				stack.pop();
+				break;
+			case "3":
+				System.out.println(Collections.max(stack));
+				break;
+			}
+		}
 	}
 }
