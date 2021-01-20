a2584bef8b39382802c1dfa653d0756311115632
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-20 16:44:43 +0200

    Exercises

--- 02_stacks_and_queues_ex/src/Exercise_06.java
+++ 02_stacks_and_queues_ex/src/Exercise_06.java
@@ -1,9 +1,33 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Exercise_06 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
-		String line = scanner.nextLine();
+		ArrayDeque<Character> openBrackets = new ArrayDeque<>();
+		
+		String input = scanner.nextLine();
+		boolean areBalanced = true;
+		for (int i = 0; i < input.length(); i++) {
+			char currentBracket = input.charAt(i);
+			
+			if(currentBracket == '{' || currentBracket == '[' || currentBracket == '(') {
+				openBrackets.push(currentBracket);
+			}else if(currentBracket == '}' || currentBracket == ']' || currentBracket == ')') {
+				char open = openBrackets.pop();
+				if(currentBracket=='}' && open!='{') {
+					areBalanced = false;
+				}else if(currentBracket==']' && open!='[') {
+					areBalanced = false;
+				}else if(currentBracket==')' && open!='(') {
+					areBalanced = false;
+				}
+			}
+		}
+		if(areBalanced) {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
+		}
 	}
 }
