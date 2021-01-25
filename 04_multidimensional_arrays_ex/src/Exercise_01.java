e9131733d22d5a93aac77e544892d461f8a8f170
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-25 18:41:34 +0200

    Exercises

--- 04_multidimensional_arrays_ex/src/Exercise_01.java
+++ 04_multidimensional_arrays_ex/src/Exercise_01.java
@@ -2,6 +2,36 @@ import java.util.Scanner;
 
 public class Exercise_01 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		String[] input = scanner.nextLine().split(", ");
+		int size = Integer.parseInt(input[0]);
+		String pattern = input[1];
+
+		int[][] matrix = new int[size][size];
+
+		int startNumber = 1;
+		if (pattern.equals("A")) {
+			for (int col = 0; col < size; col++) {
+				for (int row = 0; row < size; row++) {
+					matrix[row][col] = startNumber++;
+				}
+			}
+		} else if (pattern.equals("B")) {
+			for (int col = 0; col < size; col++) {
+				if (col % 2 == 0) {
+					for (int row = 0; row < size; row++) {
+						matrix[row][col] = startNumber++;
+					}
+				} else {
+					for (int row = size - 1; row >= 0; row--) {
+						matrix[row][col] = startNumber++;
+					}
+				}
+
+			}
+		}
+		printMatrix(matrix);
 
 	}
 
