8e05e6f2d3c1a9cbc8f254ce3e09948529813739
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-29 19:59:53 +0200

    Exercises

--- 04_multidimensional_arrays_ex/src/Exercise_04.java
+++ 04_multidimensional_arrays_ex/src/Exercise_04.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Exercise_04 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		int rows = scanner.nextInt();
+		int cols = scanner.nextInt();
+		scanner.nextLine();
+		int[][] matrix = new int[rows][cols];
+
+		for (int row = 0; row < rows; row++) {
+			matrix[row] = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+		}
+	
 	}
 }
