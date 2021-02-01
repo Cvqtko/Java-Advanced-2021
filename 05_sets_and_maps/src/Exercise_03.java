3221d14f9990c7cc081ae798e50668d470b468ce
Cvqtko <cvqtkokirilov@gmail.com>
2021-02-01 15:57:29 +0200

    Exercises

--- 05_sets_and_maps/src/Exercise_03.java
+++ 05_sets_and_maps/src/Exercise_03.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Exercise_03 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		Set<Integer> firstDeck = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).boxed()
+				.collect(Collectors.toCollection(LinkedHashSet::new));
+		String input = scanner.nextLine();
 		
+		Set<Integer> secondDeck = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt)
+				.collect(Collectors.toCollection(LinkedHashSet::new));
 	}
 }
