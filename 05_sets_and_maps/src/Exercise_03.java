132cc2d2299f3c7fc1f9f38fa46f4b8bb23c4c00
Cvqtko <cvqtkokirilov@gmail.com>
2021-02-06 20:47:08 +0200

    Exercises

--- 05_sets_and_maps/src/Exercise_03.java
+++ 05_sets_and_maps/src/Exercise_03.java
@@ -1,18 +1,30 @@
 import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Exercise_03 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		LinkedHashMap<Double, Integer> result = new LinkedHashMap<>();
+		Double[] input = Arrays.stream(scanner.nextLine().split("\\s+")).map(Double::parseDouble).toArray(Double[]::new);
+		for (Double number : input) {
 
-		Set<Integer> firstDeck = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).boxed()
-				.collect(Collectors.toCollection(LinkedHashSet::new));
-		String input = scanner.nextLine();
-		
-		Set<Integer> secondDeck = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt)
-				.collect(Collectors.toCollection(LinkedHashSet::new));
+			if (!result.containsKey(number)) {
+
+				result.put(number, 1);
+
+			} else {
+
+				result.put(number, result.get(number) + 1);
+
+			}
+
+		}
+
+		for (Double key : result.keySet()) {
+
+			System.out.println(key + " -> " + result.get(key));
+
+		}
 	}
 }
