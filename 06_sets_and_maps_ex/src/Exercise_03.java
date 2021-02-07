e0e52dfc40aff1a27367e2b1bb46e3a14399de91
Cvqtko <cvqtkokirilov@gmail.com>
2021-02-07 19:17:23 +0200

    Exercises

--- 06_sets_and_maps_ex/src/Exercise_03.java
+++ 06_sets_and_maps_ex/src/Exercise_03.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Exercise_03 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		int n = Integer.parseInt(scanner.nextLine());
+		TreeSet<String> chemicalElements = new TreeSet<>();
+		for (int i = 0; i < n; i++) {
+			String[] elements = scanner.nextLine().split("\\s+");
+			chemicalElements.addAll(Arrays.asList(elements));
+			
+		}
+		chemicalElements.forEach(e -> System.out.print(e + " "));
 	}
 }
