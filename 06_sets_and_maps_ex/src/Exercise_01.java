0854e0876722367ca56bfd4d39cd2a4c88b12e8f
Cvqtko <cvqtkokirilov@gmail.com>
2021-02-07 16:56:33 +0200

    Exercises

--- 06_sets_and_maps_ex/src/Exercise_01.java
+++ 06_sets_and_maps_ex/src/Exercise_01.java
@@ -1,6 +1,24 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
 
 public class Exercise_01 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		int n = Integer.parseInt(scanner.nextLine());
+
+		LinkedHashSet<String> names = new LinkedHashSet<>();
+
+		for (int i = 0; i < n; i++) {
+			String name = scanner.nextLine();
+			names.add(name);
+		}
+
+		names.forEach(e -> System.out.println(e));
+
+		// another print solution
+		//for (String name : names) {
+		//	System.out.println(name);
+		//}
 	}
 }
--- 06_sets_and_maps_ex/src/Exercise_02.java
+++ 06_sets_and_maps_ex/src/Exercise_02.java
@@ -1,6 +1,32 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
 
 public class Exercise_02 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		int firstSetSize = scanner.nextInt();
+
+		int secondSetSize = scanner.nextInt();
+		// second way of reading input
+		// String input = scanner.nextLine();
+		// int firstSetSize = Integer.parseInt(input.split(" ")[0]]);
+		// int secondSetSize = Integer.parseInt(input.split(" ")[1]]);
+		scanner.nextLine();
+
+		LinkedHashSet<Integer> firstSet = new LinkedHashSet<>();
+		LinkedHashSet<Integer> secondSet = new LinkedHashSet<>();
+
+		for (int i = 0; i < firstSetSize; i++) {
+			int number = Integer.parseInt(scanner.nextLine());
+			firstSet.add(number);
+		}
+		for (int i = 0; i < secondSetSize; i++) {
+			int number = Integer.parseInt(scanner.nextLine());
+			secondSet.add(number);
+		}
+
+		firstSet.retainAll(secondSet);
+		firstSet.forEach(e -> System.out.print(e + " "));
 	}
 }
