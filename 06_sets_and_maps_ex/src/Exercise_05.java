14df7b959dc2f20f1c7d59d19f68252d809c55bb
Cvqtko <cvqtkokirilov@gmail.com>
2021-02-08 13:18:21 +0200

    Exercises

--- 06_sets_and_maps_ex/src/Exercise_05.java
+++ 06_sets_and_maps_ex/src/Exercise_05.java
@@ -1,6 +1,30 @@
+import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 public class Exercise_05 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		LinkedHashMap<String, String> phonebook = new LinkedHashMap<>();
+
+		while (true) {
+			String input = scanner.nextLine();
+
+			if (input.equals("search")) {
+				String searchedName = scanner.nextLine();
+				while (!searchedName.equals("stop")) {
+					if (!phonebook.containsKey(searchedName)) {
+						System.out.println("Contact " + searchedName + " does not exist.");
+					} else {
+						System.out.printf("%s -> %s%n", searchedName, phonebook.get(searchedName));
+					}
+					searchedName = scanner.nextLine();
+				}
+				break;
+			} else {
+				String name = input.split("-")[0];
+				String number = input.split("-")[1];
+				phonebook.put(name, number);
+			}
+		}
 	}
 }
