54009b0ff22176d65df01bbba285d474c8cfb018
Cvqtko <cvqtkokirilov@gmail.com>
2021-05-29 14:40:42 +0300

    Exercises

--- 06_sets_and_maps_ex/src/Exercise_10.java
+++ 06_sets_and_maps_ex/src/Exercise_10.java
@@ -51,15 +51,14 @@ public class Exercise_10 {
 
 	private static HashMap sortByValues(Map<String, Long> map) {
 		List list = new LinkedList(map.entrySet());
-		// Defined Custom Comparator here
+
 		Collections.sort(list, new Comparator() {
 			public int compare(Object o1, Object o2) {
 				return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
 			}
 		});
 
-		// Here I am copying the sorted list in HashMap
-		// using LinkedHashMap to preserve the insertion order
+
 		HashMap sortedHashMap = new LinkedHashMap();
 		for (Iterator it = list.iterator(); it.hasNext();) {
 			Map.Entry entry = (Map.Entry) it.next();
