429b27c331787e48822744124d12d94d01de411b
Cvqtko <cvqtkokirilov@gmail.com>
2021-06-01 18:26:29 +0300

    Exercises

--- 08_streams_ex/src/Exercise_12.java
+++ 08_streams_ex/src/Exercise_12.java
@@ -1,4 +1,39 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Exercise_12 {
+	public static void main(String[] args) throws IOException {
+		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(
+				"C:\\Users\\Leet\\IdeaProjects\\Homeworkd Streams and Files\\src\\text-files.zip"));
+		FileInputStream fis = new FileInputStream(
+				new File("C:\\Users\\Leet\\IdeaProjects\\Homeworkd Streams and Files\\src\\words.txt"));
+		int byteContainter;
+		zos.putNextEntry(new ZipEntry("words.txt"));
+
+		while ((byteContainter = fis.read()) != -1) {
+			zos.write(byteContainter);
+		}
+		zos.closeEntry();
+		zos.putNextEntry(new ZipEntry("count-chars.txt"));
+		fis = new FileInputStream(
+				new File("C:\\Users\\Leet\\IdeaProjects\\Homeworkd Streams and Files\\src\\count-chars.txt"));
+		while ((byteContainter = fis.read()) != -1) {
+			zos.write(byteContainter);
+		}
+		zos.closeEntry();
+		zos.putNextEntry(new ZipEntry("lines.txt"));
+		fis = new FileInputStream(
+				new File("C:\\Users\\Leet\\IdeaProjects\\Homeworkd Streams and Files\\src\\lines.txt"));
+		while ((byteContainter = fis.read()) != -1) {
+			zos.write(byteContainter);
+		}
+		zos.closeEntry();
+		zos.finish();
+		zos.close();
+	}
 
 }
