232abe8a898035dbe184043c20a1a11e0c39d941
Cvqtko <cvqtkokirilov@gmail.com>
2021-06-14 20:18:17 +0300

    Exercises

--- 12_functional_programming_ex/src/Exercise_06.java
+++ 12_functional_programming_ex/src/Exercise_06.java
@@ -1,7 +1,16 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Exercise_06 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		int length = Integer.parseInt(scanner.nextLine());
+		String[] names = scanner.nextLine().split("\\s+");
+
+		Predicate<String> checkLength = name -> name.length() <= length;
+		
+		Arrays.stream(names).filter(checkLength).forEach(e->System.out.println(e));
 	}
 }
--- 12_functional_programming_ex/src/Exercise_08.java
+++ 12_functional_programming_ex/src/Exercise_08.java
@@ -1,7 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Exercise_08 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt)
+				.collect(Collectors.toList());
+
+		Comparator<Integer> comparator = ((a, b) -> {
+			if (a % 2 == 0 && b % 2 != 0) {
+				return -1;
+			} else if (a % 2 != 0 && b % 2 == 0) {
+				return 1;
+			} else if ((a % 2 == 0 && b % 2 == 0) || (a % 2 != 0 && b % 2 != 0)) {
+				if (a <= b) {
+					return -1;
+				} else {
+					return 1;
+				}
+			} else {
+				return 1;
+			}
+
+		});
+		numbers.sort(comparator);
+		numbers.stream().forEach(e -> System.out.print(e + " "));
 	}
 }
