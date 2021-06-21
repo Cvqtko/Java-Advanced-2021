e59af5f71abc645e7fa810402db31065251353ea
Cvqtko <cvqtkokirilov@gmail.com>
2021-06-21 15:41:38 +0300

    Exercises

--- 11_functional_programming/src/Exercise_01.java
+++ 11_functional_programming/src/Exercise_01.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Exercise_01 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		int[] numbers = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(e -> Integer.parseInt(e))
+				.filter(e -> e % 2 == 0).toArray();
+
+		String evenNumbers = Arrays.stream(numbers).mapToObj(String::valueOf).collect(Collectors.joining(", "));
+		System.out.println(evenNumbers);
+		
+		String sortedNumbers = Arrays.stream(numbers).sorted().mapToObj(String::valueOf).collect(Collectors.joining(", "));
+		System.out.println(sortedNumbers);
 	}
 
 }
