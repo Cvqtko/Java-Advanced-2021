1a7cb14e7dfc002587f50f2a7efb6ef8def3e156
Cvqtko <cvqtkokirilov@gmail.com>
2021-06-23 20:07:03 +0300

    Exercises

--- 18_generics_ex/src/ex_10/Main.java
+++ 18_generics_ex/src/ex_10/Main.java
@@ -1,29 +1,38 @@
 package ex_10;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
 		int n = 3;
 		StringBuilder sb = new StringBuilder();
+		Tuple tuple;
 		while (n-- > 0) {
 			String line = scanner.nextLine();
 			String[] tokens = line.split("\\s+");
-			Tuple tuple = null;
-			if (tokens.length > 2) {
-				String name = tokens[0] + " " + tokens[1];
+			tuple = null;
+			String name;
+			if (n == 2) {
+				name = tokens[0] + " " + tokens[1];
 				String address = tokens[2];
-				tuple = new Tuple(name, address);
+				tuple = new Tuple(name,address);
+			} else if (n == 1) {
+				name = tokens[0];
+				int liters = Integer.parseInt(tokens[1]);
+				tuple = new Tuple(name,liters);
 			}else {
-				String name = tokens[0];
-				String number = tokens[1];
-				tuple = new Tuple(name, number);
+				int intParam = Integer.parseInt(tokens[0]);
+				double doubleParam=  Double.parseDouble(tokens[1]);
+				tuple = new Tuple(intParam,doubleParam);
 			}
 			sb.append(tuple.toString()).append(System.lineSeparator());
 		}
 		System.out.println(sb.toString());
+
 	}
 
 }
