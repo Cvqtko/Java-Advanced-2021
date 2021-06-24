72cdbdb4fa928384d43690613c2bb145f4693f4f
Cvqtko <cvqtkokirilov@gmail.com>
2021-06-24 19:36:01 +0300

    Exercises

---
+++ 20_iterators_comparators_ex/src/ex_01/ListyIterator.java
@@ -0,0 +1,25 @@
+package ex_01;
+
+import java.util.List;
+
+public class ListyIterator {
+	private List<String> data;
+	private int index;
+
+	public ListyIterator(List<String> data) {
+		this.data = data;
+		if (this.data.size() != 0) {
+			this.index = 0;
+		} else {
+			this.index = -1;
+		}
+	}
+
+	public boolean move() {
+		if (this.index < this.data.size() - 1) {
+			this.index++;
+			return true;
+		}
+		return false;
+	}
+}
---
+++ 20_iterators_comparators_ex/src/ex_01/Main.java
@@ -0,0 +1,13 @@
+package ex_01;
+
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
