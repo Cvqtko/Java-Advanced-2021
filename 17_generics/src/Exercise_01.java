7f0ad3730a0f3088362371176d7a023172dfde84
Cvqtko <cvqtkokirilov@gmail.com>
2021-06-28 16:17:17 +0300

    Exercises

---
+++ 17_generics/src/Exercise_01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercise_01 {
+	public static void main(String[] args) {
+		Jar jar = new Jar();
+		jar.add(12);
+		
+		System.out.println(jar.remove());
+	}
+}
